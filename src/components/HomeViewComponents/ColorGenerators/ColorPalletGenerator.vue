54918ffbeaa4d13e7e17a847ff3e40df9974279d
JarneSt <12001144@student.pxl.be>
2022-07-10 11:45:37 +0200

    Color Pallet Saves

--- src/components/HomeViewComponents/ColorGenerators/ColorPalletGenerator.vue
+++ src/components/HomeViewComponents/ColorGenerators/ColorPalletGenerator.vue
@@ -4,35 +4,54 @@
             <n-space :style="{ display: 'block' }">
                 <div class="colorPalletGenerator d-flex justify-content-between" style="gap:25px;">
                     <n-card class="w-50" title="Color lighter/darkener">
-                        <n-color-picker v-model:value="color" :modes="['hex']" placeholder="Enter a color">
+                        <n-color-picker v-model:value="color" :modes="['hex']"
+                            :on-complete="generateDarkOrLighterColor(color, shadePercentage)"
+                            placeholder="Enter a color" :swatches="[
+                                '#00FF78FF',
+                                '#0076FFFF',
+                                '#FF9D00FF',
+                                '#FF0000FF',
+                                '#A700EEFF',
+                            ]" :show-preview="true">
                         </n-color-picker>
-                        <n-input-number v-model:value="shadePercentage" placeholder="Enter amount ">
+
+
+                        <n-radio-group @change="handleChange" name="radiobuttongroup1" class="d-flex mb-2">
+                            <n-radio-button class="w-100" :value="'Lighter'" :label="'Lighter'" />
+                            <n-radio-button class="w-100" :value="'Darker'" :label="'Darker'" />
+                        </n-radio-group>
+
+
+                        <n-input-number @keyup="generateDarkOrLighterColor(color, shadePercentage)"
+                            v-model:value="shadePercentage" placeholder="Enter amount ">
                             <template #suffix>
                                 %
                             </template>
                         </n-input-number>
 
-                        <n-space class="p-3 d-flex justify-content-between">
-                            <n-radio :checked="checkedValue === 'Lighter'" value="Lighter" name="basic-demo"
-                                @change="handleChange">
-                                Lighter
-                            </n-radio>
-                            <n-radio :checked="checkedValue === 'Darker'" value="Darker" name="basic-demo"
-                                @change="handleChange">
-                                Darker
-                            </n-radio>
-                        </n-space>
 
-                        <n-button class="w-100" @click="generateDarkOrLighterColor(color, shadePercentage)">Apply
-                        </n-button>
 
-                        <h4 class="mt-5 pb-3">Result</h4>
-                        <n-color-picker v-model:value="shadedColor"></n-color-picker>
+                        <!-- <n-button class="w-100" @click="generateDarkOrLighterColor(color, shadePercentage)">Apply
+                        </n-button> -->
+                        <n-card title="Result" class="mt-3">
+                            <n-color-picker v-model:value="shadedColor" :modes="['hex']" :show-preview="true" disabled>
+                            </n-color-picker>
+                            <n-button class="w-100" @click="copyHEXToClipboard(shadedColor)">Copy to clipboard
+                            </n-button>
+                        </n-card>
+                        <!-- <h4 class="mt-5 pb-3">Result</h4>
+                        <n-color-picker v-model:value="shadedColor" :modes="['hex']" :show-preview="true">
+                        </n-color-picker> -->
 
                     </n-card>
                     <n-card class="w-50" title="Color Pallet Generator">
-                        <n-color-picker v-model:value="selectedColorForPallet" :modes="['hex']"
-                            placeholder="Enter a color">
+                        <n-color-picker v-model:value="selectedColorForPallet" :swatches="[
+                            '#00FF78FF',
+                            '#0076FFFF',
+                            '#FF9D00FF',
+                            '#FF0000FF',
+                            '#A700EEFF',
+                        ]" :modes="['hex']" :show-preview="true" placeholder="Enter a color">
                         </n-color-picker>
                         <n-button class="w-100" @click="generateColorPallet()">Generate color pallet</n-button>
                     </n-card>
@@ -40,30 +59,48 @@
                 </div>
             </n-space>
         </n-card>
+
+        <n-card title="Saved Color Pallets">
+            <section>
+                <n-card v-for="color of savedColorPallets" :key="color"
+                    :title="'Color pallet' + ' build from HEX: ' + color[0]">
+                    <div v-for="cl in color" :key="cl" :style="{ background: cl }" class="colorPalletItem">
+                        <div class="d-flex justify-content-between w-100">
+                            <p class="d-flex m-auto">{{ cl.toUpperCase() }}</p>
+                            <n-button :style="{ backgroundColor: '#2C2C32' }" class="m-2"
+                                @click="copyHEXToClipboard(cl)">Copy HEX</n-button>
+                        </div>
+                    </div>
+                </n-card>
+            </section>
+        </n-card>
     </n-space>
 
-    <n-drawer v-model:show="showColorPalletDrawer" :width="'70%'" :height="200" :placement="'left'" closable
-        :trap-focus="false">
-        <n-drawer-content title="Color Pallet">
+    <n-drawer :style="{ minWidth: '500px' }" v-model:show="showColorPalletDrawer" :default-width="500"
+        :placement="'left'" :trap-focus="false" resizable>
+        <n-drawer-content title="Color Pallet" closable>
             <section id="colorPalletContent">
                 <div v-for="color in colorPallet" :key="color" :style="{ background: color }" class="colorPalletItem">
                     <div class="d-flex justify-content-between w-100">
-                        <p class="d-flex m-auto">{{ color }}</p>
+                        <p class="d-flex m-auto">{{ color.toUpperCase() }}</p>
                         <n-button :style="{ backgroundColor: '#2C2C32' }" class="m-2"
                             @click="copyHEXToClipboard(color)">Copy HEX</n-button>
                     </div>
                 </div>
             </section>
+            <template #footer>
+                <n-button @click="saveColorPallet(colorPallet)">Save color pallet</n-button>
+            </template>
         </n-drawer-content>
     </n-drawer>
 </template>
 
 <script>
 import { ref } from "vue";
-import { useMessage, NButton, NInput, NColorPicker, NCard, NSpace, NInputNumber, NRadio, NDrawer, NDrawerContent } from "naive-ui";
+import { useMessage, NButton, NInput, NColorPicker, NCard, NSpace, NInputNumber, NRadio, NDrawer, NDrawerContent, NRadioGroup, NRadioButton } from "naive-ui";
 export default {
     components: {
-        NButton, NInput, NColorPicker, NCard, NSpace, NInputNumber, NRadio, NDrawer, NDrawerContent
+        NButton, NInput, NColorPicker, NCard, NSpace, NInputNumber, NRadio, NDrawer, NDrawerContent, NRadioGroup, NRadioButton
     },
     data() {
         return {
@@ -73,27 +110,40 @@ export default {
             shadePercentage: undefined,
             showColorPalletDrawer: false,
             lighter: false,
-            colorPallet: []
+            colorPallet: [],
+            savedColorPallets: [],
         }
     },
     mounted() {
         window.$message = useMessage();
     },
     methods: {
+        saveColorPallet(colorPallet) {
+            let obj = {};
+            let index = 0;
+            for (let color of colorPallet) {
+                obj[index] = color;
+                index++;
+            }
+            this.savedColorPallets.push(obj);
+            this.showColorPalletDrawer = false;
+        },
         copyHEXToClipboard(color) {
             navigator.clipboard.writeText(color);
-            window.$message.success('HEX Copied to clipboard');
+            window.$message.success(color.toUpperCase() + ' Copied to clipboard');
         },
         generateDarkOrLighterColor(color, shadePercentage) {
             let shadeColor = this.shadeColor(color, shadePercentage);
             this.shadedColor = shadeColor;
         },
         generateColorPallet() {
-            let colors = [];;
+            let colors = [];
+            colors.push(this.selectedColorForPallet);
             for (let i = 0; i < 10; i++) {
                 let color = this.shadeColor(this.selectedColorForPallet, i * 10);
                 colors.push(color);
             }
+
 
             this.colorPallet = colors.filter((item, index) => colors.indexOf(item) === index);
             this.showColorPalletDrawer = true;
