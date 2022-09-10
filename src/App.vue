56f3240d485a7f5e4d81837b75b8495b4c0f256e
JarneStaalPXL <jarne.staal9@outlook.com>
2022-09-10 11:19:04 +0200

    Dark switch fix v2

--- src/App.vue
+++ src/App.vue
@@ -200,7 +200,7 @@ export default {
   data() {
     return {
       collapsed: false,
-      switchIsChecked: undefined,
+      switchIsChecked: false,
       isScrollingDown: false,
       isScrollingUp: false,
       lastScrollY: 0,
