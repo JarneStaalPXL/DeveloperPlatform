d47938d86e33d81b9adbdf66497c65eff8f36eb5
JarneStaalPXL <jarne.staal9@outlook.com>
2022-09-13 19:50:42 +0200

    Reactive fix for Menu header

--- src/components/ProfileViewComponents/ProfileInfo.vue
+++ src/components/ProfileViewComponents/ProfileInfo.vue
@@ -31,30 +31,6 @@
               content="Visited Route"
               :time="item.createdAt"
             />
-            <!-- <n-timeline-item content="Oops" />
-            <n-timeline-item
-              type="success"
-              title="Success"
-              content="Success content"
-              time="2018-04-03 20:46"
-            />
-            <n-timeline-item
-              type="error"
-              content="Error content"
-              time="2018-04-03 20:46"
-            />
-            <n-timeline-item
-              type="warning"
-              title="Warning"
-              content="Warning content"
-              time="2018-04-03 20:46"
-            />
-            <n-timeline-item
-              type="info"
-              title="Info"
-              content="Info content"
-              time="2018-04-03 20:46"
-            /> -->
           </n-timeline>
         </n-scrollbar>
       </n-card>
