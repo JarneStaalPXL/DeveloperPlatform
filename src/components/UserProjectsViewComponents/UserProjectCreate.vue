2bbd32c7ba626c6422a4849bf6db1d072bb4cd89
DeveloperJarne <12001144@student.pxl.be>
2022-10-17 15:17:01 +0200

    userProjectCreation components

---
+++ src/components/UserProjectsViewComponents/UserProjectCreate.vue
@@ -0,0 +1,38 @@
+<template>
+    <n-space vertical class="d-flex">
+        <n-input placeholder="Project Title" v-model:value="project.title"/>
+        <n-input placeholder="Project Description" v-model:value="project.description"/>
+        <n-input placeholder="Project Image" v-model:value="project.image"/>
+        <n-input placeholder="Project Website Link" v-model:value="project.websiteLink"/>
+        <n-input placeholder="GitHub Repository" v-model:value="project.github"/>
+        <n-button class="w-100">Create project</n-button>
+    </n-space>
+</template>
+
+<script>
+import {
+    NButton, NInput, NSpace
+} from "naive-ui"
+export default {
+  components: { 
+    NButton, NInput, NSpace
+  
+  },
+  data(){
+    return {
+        project: {
+            title: "",
+            description: "",
+            image: "",
+            websiteLink: "",
+            github: "",
+            technologies: []
+        }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
